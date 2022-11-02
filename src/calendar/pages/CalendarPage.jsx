5a2e279887ddf19b5e4d973642f27b2803a9afb4
JaacielBriseo <jaacielbris23@gmail.com>
2022-11-01 18:25:02 -0700

    React-big-calendar installed

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,41 @@
-import { Navbar } from "../"
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import { addHours,format,parse,startOfWeek,getDay } from 'date-fns';
+import enUS from 'date-fns/locale/en-US';
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import { Navbar } from '../';
 
+const locales = {
+  'en-US': enUS,
+};
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+const events = [{
+  title: 'Cumpleaños del jefe',
+  notes: 'Hay que comprar el pastel',
+  start: new Date(),
+  end: addHours(new Date(), 2),
+  bgColor: '#fafafa',
+  user:{
+    _id:'123',
+    name: 'Jaaciel'
+  }
+}]
 export const CalendarPage = () => {
   return (
     <>
-    <Navbar/>
+      <Navbar />
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc( 100vh - 80px )' }}
+      />
     </>
-  )
-}
+  );
+};

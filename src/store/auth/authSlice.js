695761a413455b468444f3d47fc62973fecbaaad
JaacielBriseo <jaacielbris23@gmail.com>
2022-11-07 12:25:08 -0700

    Dispatch actions on useAuthStore / sweetalert on LoginPage

--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -1,25 +1,30 @@
 import { createSlice } from '@reduxjs/toolkit';
 export const authSlice = createSlice({
-    name: 'auth',
-    initialState: {
-        status: 'checking',
-        user:{},
-        errorMessage: undefined,
+  name: 'auth',
+  initialState: {
+    status: 'checking',
+    user: {},
+    errorMessage: undefined,
+  },
+  reducers: {
+    onChecking: (state) => {
+      state.status = 'checking';
+      (state.user = {}), (state.errorMessage = undefined);
     },
-    reducers: {
-        onChecking: (state) => {
-            state.status = 'checking';
-            state.user = {},
-            state.errorMessage = undefined
-        },
-        onLogin: (state, {payload}) => {
-            state.status = 'authenticated',
-            state.user = payload;
-            state.errorMessage = undefined;
-        }
-    }
+    onLogin: (state, { payload }) => {
+      (state.status = 'authenticated'), (state.user = payload);
+      state.errorMessage = undefined;
+    },
+    onLogout: (state, { payload }) => {
+      (state.status = 'not-authenticated'), (state.user = {});
+      state.errorMessage = payload;
+    },
+    clearErrorMessage: (state) => {
+      state.errorMessage = undefined;
+    },
+  },
 });
 
-
 // Action creators are generated for each case reducer function
-export const { onChecking,onLogin } = authSlice.actions;
+export const { onChecking, onLogin, onLogout, clearErrorMessage } =
+  authSlice.actions;

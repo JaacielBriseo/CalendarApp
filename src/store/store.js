8b28e81a0626fe204bdf963eec9390a9aad2a1ed
JaacielBriseo <jaacielbris23@gmail.com>
2022-11-07 10:46:07 -0700

    Initial config of authSlice w/redux-toolkit

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,5 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
-import { uiSlice, calendarSlice } from "./";
+import { configureStore } from "@reduxjs/toolkit";
+import { uiSlice, calendarSlice, authSlice } from "./";
 
 export const store = configureStore({
     middleware: getDefaultMiddleware =>
@@ -9,5 +9,6 @@ export const store = configureStore({
     reducer:{
         calendar: calendarSlice.reducer,
         ui: uiSlice.reducer,
+        auth: authSlice.reducer
     }
 })

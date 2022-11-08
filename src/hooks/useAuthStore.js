689400e1774076c68cf661e689256c8849708fdc
JaacielBriseo <jaacielbris23@gmail.com>
2022-11-07 21:42:44 -0700

    checkAuthToken on useAuthStore / interceptor to calendarApi

--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -39,6 +39,20 @@ export const useAuthStore = () => {
       }, 10);
     }
   };
+
+  const checkAuthToken = async () => {
+    const token = localStorage.getItem('token');
+    if (!token) return dispatch(onLogout());
+    try {
+      const { data } = await calendarApi.get('auth/renew');
+      localStorage.setItem('token', data.token);
+      localStorage.setItem('token-init-date', new Date().getTime());
+      dispatch(onLogin({ name: data.name, uid: data.uid }));
+    } catch (error) {
+      localStorage.clear();
+      dispatch(onLogout());
+    }
+  };
   return {
     //* Propiedades
     status,
@@ -47,5 +61,6 @@ export const useAuthStore = () => {
     //*Métodos
     startLogin,
     startRegister,
+    checkAuthToken,
   };
 };
